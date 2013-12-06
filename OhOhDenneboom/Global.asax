3bae870c91a2b8e9b7df0c26310b9ef17b39eb8b
laurensdewit <dewit.laurens@gmail.com>
2013-12-06 12:25:52 +0100

    HashBang!
    Olalalalala

---
+++ OhOhDenneboom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OhOhDenneboom.MvcApplication" Language="C#" %>
---
+++ OhOhDenneboom/Models/domein/Boom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace OhOhDenneboom
+{
+    public class Boom
+    {
+        private int Score;
+
+        public Boom()
+        {
+            throw new System.NotImplementedException();
+        }
+    
+        public int Id
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Naam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void UpVote()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
